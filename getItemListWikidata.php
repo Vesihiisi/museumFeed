84bbd6cce8497614592dd3925d8876026ec680fc
Vesihiisi <alicia@fagerving.se>
2016-09-21 20:54:56 +0200

    rewrite to use SPARQL instead of WDQ

--- getItemListWikidata.php
+++ getItemListWikidata.php
@@ -1,9 +1,8 @@
 <?php
 
-include("curl.php");
-
 $countryItem = "183";
 
-$data = curl("http://wdq.wmflabs.org/api?q=claim[31:(TREE[33506][][279])]%20AND%20tree[" . $countryItem ."][150][17,131]%20AND%20claim[2003]");
 
-echo $data;
+$data = file_get_contents("http://query.wikidata.org/bigdata/namespace/wdq/sparql?query=SELECT%20DISTINCT%20%3Fmuseum%20%3FmuseumLabel%20%3F_Instagram_username%20WHERE%20%7B%0A%20%20%3Fmuseum%20wdt%3AP31%2Fwdt%3AP279*%20wd%3AQ33506%20.%0A%20%20%3Fmuseum%20p%3AP2003%20%3Fstatement.%0A%20%20%3Fmuseum%20%3Frange%20wd%3AQ183.%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22sv%22.%20%7D%0A%20%20OPTIONAL%20%7B%20%3Fmuseum%20wdt%3AP2003%20%3F_Instagram_username.%20%7D%0A%7D&format=json");
+
+echo($data);
--- js/script.js
+++ js/script.js
@@ -18,8 +18,7 @@ $(document).ready(function() {
             url: url,
             success: function(data) {
                 console.log("downloaded from " + url)
-                var itemsList = data.items;
-                displayPhotos(itemsList);
+                displayPhotos(data.results.bindings);
             }
         });
     }
@@ -27,6 +26,39 @@ $(document).ready(function() {
 
     function displayPhotos(itemList) {
         for (var i = 0; i < itemList.length; i++) {
+            var instaUsername = itemList[i]._Instagram_username.value;
+            console.log(instaUsername)
+
+                                $.ajax({
+                        type: "POST",
+                        data: {
+                            json: instaUsername
+                        },
+                        url: "insta.php",
+                        success: function(data) {
+                            var photos = jQuery.parseJSON(data).items.slice(0, 10)
+                            for (var i = 0; i < photos.length; i++) {
+                                var photo = {
+                                    username: photos[i].user.full_name,
+                                    timestamp: photos[i].created_time,
+                                    caption: photos[i].caption.text,
+                                    url: photos[i].images.low_resolution.url,
+                                    note: " "
+                                }
+                                picsToDisplay.push(photo);
+                                picsToDisplay.sort(function(a, b) {
+                                    return parseInt(b.timestamp) - parseInt(a.timestamp);
+                                });
+                                $('#photobox li:eq(' + picsToDisplay.indexOf(photo) + ')').hide().after('<li class=imgli>' +  "<img src=" + photo.url + " class='photo' id=" + photo.timestamp + " title='" + timestampProcess(photo.timestamp) + "'>" + '</li>').fadeIn();
+                            }
+
+                        }
+                    })
+
+
+                                
+        }
+/*        for (var i = 0; i < itemList.length; i++) {
             $.ajax({
                 dataType: "jsonp",
                 url: "http://www.wikidata.org/w/api.php?action=wbgetclaims&entity=Q" + itemList[i] + "&property=P2003&format=json",
@@ -62,7 +94,7 @@ $(document).ready(function() {
 
                 }
             })
-        }
+        }*/
     }
 
     displayPhotosFromDatabaseFeed();
