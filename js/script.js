0b4004ca68ea48407ef26572386ad746cb27eb5f
Vesihiisi <alicia@fagerving.se>
2016-09-21 21:28:39 +0200

    clean up, limit no of pics, rm blinking

--- js/script.js
+++ js/script.js
@@ -29,72 +29,35 @@ $(document).ready(function() {
             var instaUsername = itemList[i]._Instagram_username.value;
             console.log(instaUsername)
 
-                                $.ajax({
-                        type: "POST",
-                        data: {
-                            json: instaUsername
-                        },
-                        url: "insta.php",
-                        success: function(data) {
-                            var photos = jQuery.parseJSON(data).items.slice(0, 10)
-                            for (var i = 0; i < photos.length; i++) {
-                                var photo = {
-                                    username: photos[i].user.full_name,
-                                    timestamp: photos[i].created_time,
-                                    caption: photos[i].caption.text,
-                                    url: photos[i].images.low_resolution.url,
-                                    note: " "
-                                }
-                                picsToDisplay.push(photo);
-                                picsToDisplay.sort(function(a, b) {
-                                    return parseInt(b.timestamp) - parseInt(a.timestamp);
-                                });
-                                $('#photobox li:eq(' + picsToDisplay.indexOf(photo) + ')').hide().after('<li class=imgli>' +  "<img src=" + photo.url + " class='photo' id=" + photo.timestamp + " title='" + timestampProcess(photo.timestamp) + "'>" + '</li>').fadeIn();
-                            }
-
-                        }
-                    })
-
-
-                                
-        }
-/*        for (var i = 0; i < itemList.length; i++) {
             $.ajax({
-                dataType: "jsonp",
-                url: "http://www.wikidata.org/w/api.php?action=wbgetclaims&entity=Q" + itemList[i] + "&property=P2003&format=json",
+                type: "POST",
+                data: {
+                    json: instaUsername
+                },
+                url: "insta.php",
                 success: function(data) {
-                    var instaUsername = data.claims["P2003"][0].mainsnak.datavalue.value;
-                    console.log(instaUsername)
-                    $.ajax({
-                        type: "POST",
-                        data: {
-                            json: instaUsername
-                        },
-                        url: "insta.php",
-                        success: function(data) {
-                            var photos = jQuery.parseJSON(data).items.slice(0, 10)
-                            for (var i = 0; i < photos.length; i++) {
-                                var photo = {
-                                    username: photos[i].user.full_name,
-                                    timestamp: photos[i].created_time,
-                                    caption: photos[i].caption.text,
-                                    url: photos[i].images.low_resolution.url,
-                                    note: " "
-                                }
-                                picsToDisplay.push(photo);
-                                picsToDisplay.sort(function(a, b) {
-                                    return parseInt(b.timestamp) - parseInt(a.timestamp);
-                                });
-                                $('#photobox li:eq(' + picsToDisplay.indexOf(photo) + ')').hide().after('<li class=imgli>' +  "<img src=" + photo.url + " class='photo' id=" + photo.timestamp + " title='" + timestampProcess(photo.timestamp) + "'>" + '</li>').fadeIn();
-                            }
-
+                    var photos = jQuery.parseJSON(data).items.slice(0, 10)
+                    for (var i = 0; i < 5; i++) {
+                        var photo = {
+                            username: photos[i].user.full_name,
+                            timestamp: photos[i].created_time,
+                            caption: photos[i].caption.text,
+                            url: photos[i].images.low_resolution.url,
+                            note: " "
                         }
-                    })
-
+                        picsToDisplay.push(photo);
+                        picsToDisplay.sort(function(a, b) {
+                            return parseInt(b.timestamp) - parseInt(a.timestamp);
+                        });
+                        $('#photobox li:eq(' + picsToDisplay.indexOf(photo) + ')').after('<li class="imgli">' + "<img src=" + photo.url + " class='photo' id=" + photo.timestamp + " title='" + timestampProcess(photo.timestamp) + "'>" + '</li>');
+                    }
 
                 }
             })
-        }*/
+
+
+
+        }
     }
 
     displayPhotosFromDatabaseFeed();
